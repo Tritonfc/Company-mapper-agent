5cf3fc36eaeedc87a0f7d7c935b0d2f69d12a354
Tobe Iheme <ihemeprogress@gmail.com>
2026-04-14 22:39:52 +0300

    added load_env

--- src/sumble/client.py
+++ src/sumble/client.py
@@ -1,7 +1,11 @@
 import os
 import httpx
+import logging
+from dotenv import load_dotenv
 
 from src.sumble.models import JobSearchCriteria
+
+load_dotenv()
 
 BASE_URL = "https://api.sumble.com/v5"
 token = os.getenv("SUMBLE_API_KEY")
@@ -39,6 +43,14 @@ def find_organizations(
         body["order_by_column"] = order_by_column
     if order_by_direction:
         body["order_by_direction"] = order_by_direction
+        
+        
+    logging.basicConfig(
+    format="%(levelname)s [%(asctime)s] %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.DEBUG
+)
+
 
     response = httpx.post(
         url=f"{BASE_URL}/organizations/find",
